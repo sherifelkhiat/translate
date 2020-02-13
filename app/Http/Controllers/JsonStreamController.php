89c7e60392d50144619cab94cf17d52795528f27
Sherif elkhiat <sherif.elkhiat@gmail.com>
2020-02-13 10:42:10 +0200

    First commit

---
+++ app/Http/Controllers/JsonStreamController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Name;
+use Illuminate\Http\Request;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+use DB;
+
+class JsonStreamController extends Controller
+{
+    protected $names;
+
+    protected $translatedNames;
+
+    public function stream()
+    {
+        $samplefile = storage_path('sample.json');
+
+        /*
+        * JSON streaming is an efficient, fast JSON stream parser based on generators developed for unpredictably long JSON streams or documents.
+        */
+
+        $jsonStream = \JsonMachine\JsonMachine::fromFile($samplefile);
+
+        foreach ($jsonStream as $name => $data) {
+            $translatedNames = $this->translate($data);
+        }
+    }
+
+    public function translate($data)
+    {
+        $names = explode(",",trim($data['names'],'[]'));
+
+        $hits = explode(",",trim($data['hits'],'[]'));
+
+        $tr = new GoogleTranslate();
+
+        foreach($names as $key => $name){
+            $selectedName = DB::table('names')->where('name', $name)->first();
+
+            $nameId = DB::table('names')->insertGetId(
+                ['name' => $name, 'hit' => $hits[$key]]
+            );
+
+            // if duplicate
+            if($selectedName){
+                DB::table('names')
+                    ->where('translated_name_id', $selectedName->id)
+                    ->increment('hit', 1);
+            } else {
+                $hit = $hits[$key]++; 
+
+                //Detect language
+                $tr->translate($name);
+    
+                if($tr->getLastDetectedSource() != 'ar'){
+                    $this->translateAndSave($tr, $name, $hit, $nameId, 'en');
+                }else{
+                    $this->translateAndSave($tr, $name, $hit, $nameId, 'ar');
+                }
+            }
+        }
+    }
+
+    public function translateAndSave($tr, $name, $hit, $nameId, $targetLang)
+    {
+        if($targetLang == 'ar'){
+            $translatedName = $tr->setSource('ar')->setTarget('en')->translate($name);
+        } else {
+            $translatedName = $tr->setSource('en')->setTarget('ar')->translate($name);
+        }
+
+        DB::table('names')->insert(
+            ['name' => $translatedName, 'hit' => $hit, 'translated_name_id' => $nameId]
+        );
+    }
+}
---
+++ database/migrations/2020_02_11_200749_create_names_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('names', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('hit')->nullable();
+            $table->unsignedBigInteger('translated_name_id')->nullable();
+            $table->foreign('translated_name_id')->references('id')->on('names');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('names');
+    }
+}
